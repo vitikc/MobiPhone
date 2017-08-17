b8304b8f3dbd7cba70b6b677eab35e09b98236e3
vitikc <vitikc@gmail.com>
2017-08-17 22:19:01 +0300

    Realized sending and receiving messages.

--- src/me/vitikc/mobiphone/commands/MPPhoneCommand.java
+++ src/me/vitikc/mobiphone/commands/MPPhoneCommand.java
@@ -1,7 +1,10 @@
 package me.vitikc.mobiphone.commands;
 
 import me.vitikc.mobiphone.MPMain;
+import me.vitikc.mobiphone.MPPhonesManager;
 import me.vitikc.mobiphone.contacts.MPContactsManager;
+import me.vitikc.mobiphone.messages.MPMessagesManager;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,6 +13,8 @@ import org.bukkit.entity.Player;
 public class MPPhoneCommand implements CommandExecutor {
 
     private MPContactsManager cm = MPMain.getInstance().getContactsManager();
+    private MPMessagesManager mm = MPMain.getInstance().getMessagesManager();
+    private MPPhonesManager pm = MPMain.getInstance().getPhonesManager();
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -46,7 +51,24 @@ public class MPPhoneCommand implements CommandExecutor {
                 case "number": //number <player> - Show player's phone number
                     break;
                 case "sms": //sms list - Shows sent messages
+                    if (args[1].equalsIgnoreCase("list"))
+                        player.sendMessage(mm.getMessages(player.getUniqueId()));
                     break;
+                case "register":
+                    if (pm.isRegistered(player.getName())){
+                        player.sendMessage("You already registered a number!");
+                        return true;
+                    }
+                    if (!cm.isValidNumber(args[1])){
+                        player.sendMessage("Not valid number!");
+                        return true;
+                    }
+                    if (pm.isNumberExist(args[1])){
+                        player.sendMessage("Number already taken!");
+                        return true;
+                    }
+                    pm.addPlayer(args[1], player.getName());
+                    player.sendMessage("Now your phone number is " + args[1]);
                 default:
                     break;
             }
@@ -61,7 +83,7 @@ public class MPPhoneCommand implements CommandExecutor {
                         }
                         player.sendMessage(args[2] + " : " + number);
                     } else if (args[1].equalsIgnoreCase("remove")){ //remove <contact_name> deletes contact
-                        if (!cm.isContainsContact(player.getUniqueId(), args[2].toLowerCase())){
+                        if (!cm.isContainsContactName(player.getUniqueId(), args[2].toLowerCase())){
                             player.sendMessage("Contact " + args[2] + " not found");
                             return true;
                         }
@@ -76,7 +98,7 @@ public class MPPhoneCommand implements CommandExecutor {
             switch (args[0].toLowerCase()){
                 case "contacts":
                     if (args[1].equalsIgnoreCase("add")){ //add <contact_name> <number> add's contact
-                        if (cm.isContainsContact(player.getUniqueId(), args[2].toLowerCase())){
+                        if (cm.isContainsContactName(player.getUniqueId(), args[2].toLowerCase())){
                             player.sendMessage("Already have contact " + args[2]);
                             return true;
                         }
@@ -84,17 +106,86 @@ public class MPPhoneCommand implements CommandExecutor {
                             player.sendMessage(args[3] + " not valid for number");
                             return true;
                         }
+                        if (!pm.isNumberExist(args[3])){
+                            player.sendMessage("Number not found!");
+                            return true;
+                        }
+                        if (pm.getNumber(player.getName()).equals(args[3])){
+                            player.sendMessage("Can't add my number to contacts");
+                            return true;
+                        }
+                        if (cm.isContainsContactNumber(player.getUniqueId(), args[3])){
+                            player.sendMessage("Already added contact with this number: "
+                                    + cm.getContact(player.getUniqueId(), args[3]));
+                            return true;
+                        }
                         cm.addContact(player.getUniqueId(),args[2].toLowerCase(),args[3]);
                     }
-                    break;
                 case "sms":
-                    if (args[1].equalsIgnoreCase("send")){ //send <contact_name> <text> send sms to contact
-
-                    }
+                    if (args[1].equalsIgnoreCase("send"))
+                        sendSms(player, args);
+                    break;
                 default:
                     break;
             }
+        } else if (args.length > 4){
+            if (!args[0].equalsIgnoreCase("sms")){
+                return true;
+            }
+            if (!args[1].equalsIgnoreCase("send")){
+                return true;
+            }
+            sendSms(player, args);
         }
         return true;
+    }
+
+    private void sendSms(Player player, String args[]){
+        if (!pm.isRegistered(player.getName())){
+            player.sendMessage("Register your number! /phone register <number>");
+            return;
+        }
+        StringBuilder message = new StringBuilder();
+        for (int i = 3; i < args.length; i++){
+            message.append(args[i])
+                    .append(" ");
+        }
+        message.replace(message.length()-1,message.length(),"");
+        if (message.toString().length() > 100){
+            player.sendMessage("Too many characters in sms!");
+            return;
+        }
+        boolean isCheckByNumber = false;
+        if (cm.isValidNumber(args[2]))
+            isCheckByNumber = true;
+        if (!isCheckByNumber) {
+            if (!cm.isContainsContactName(player.getUniqueId(), args[2])) {
+                player.sendMessage(args[2] + " contact not found!");
+                return;
+            }
+        } else {
+            if (!cm.isValidNumber(args[2])){
+                player.sendMessage(args[2] + " number not found!");
+                return;
+            }
+        }
+        Player receiver;
+        if (isCheckByNumber)
+            receiver = Bukkit.getPlayer(pm.getPlayer(args[2]));
+        else {
+            receiver = Bukkit.getPlayer(pm.getPlayer(cm.getNumber(player.getUniqueId(), args[2])));
+        }
+        if (receiver == null || !receiver.isOnline()){
+            player.sendMessage("Contact phone is offline!");
+            return;
+        }
+        mm.sendMessage(player.getUniqueId(), receiver.getName(), message.toString());
+        mm.receiveMessage(receiver.getUniqueId(), player.getName(), message.toString());
+        player.sendMessage("Sms sent to " + args[2]);
+        String senderNumber = pm.getNumber(player.getName());
+        if (cm.isContainsContactNumber(receiver.getUniqueId(), senderNumber))
+            receiver.sendMessage("New sms from " + cm.getContact(receiver.getUniqueId(), senderNumber));
+        else
+            receiver.sendMessage("New sms from " + senderNumber);
     }
 }
--- src/me/vitikc/mobiphone/messages/MPMessagesManager.java
+++ src/me/vitikc/mobiphone/messages/MPMessagesManager.java
@@ -1,4 +1,108 @@
 package me.vitikc.mobiphone.messages;
 
+import me.vitikc.mobiphone.MPMain;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
 public class MPMessagesManager{
+    private File messagesFile;
+    private YamlConfiguration messages;
+
+    public MPMessagesManager(){
+        messagesFile = new File(MPMain.getInstance().getDataFolder(), "messages.yml");
+        messages = YamlConfiguration.loadConfiguration(messagesFile);
+        if (!messagesFile.exists())
+            saveMessages();
+    }
+
+    private void saveMessages(){
+        try {
+            messages.save(messagesFile);
+        } catch (IOException e) {
+            MPMain.getInstance().getLogger().info("Can't save messages.yml");
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(UUID holder, String to, String msg){
+        int id = 0;
+        if (messages.isSet(holder.toString() + ".sent")){
+            ConfigurationSection section = messages.getConfigurationSection(holder.toString() + ".sent");
+            ArrayList<String> list = new ArrayList<>();
+            list.addAll(section.getKeys(false));
+            id = list.size();
+        }
+        if (id > 10) {
+            id = 0;
+            messages.set(holder.toString() + ".sent", null);
+        }
+        messages.set(holder.toString() + ".sent." + id + ".to", to);
+        messages.set(holder.toString() + ".sent." + id + ".msg", msg);
+        saveMessages();
+    }
+
+    public void receiveMessage(UUID holder, String from, String msg){
+        int id = 0;
+        if (messages.isSet(holder.toString() + ".received")){
+            ConfigurationSection section = messages.getConfigurationSection(holder.toString() + ".received");
+            ArrayList<String> list = new ArrayList<>();
+            list.addAll(section.getKeys(false));
+            id = list.size();
+        }
+        if (id > 10) {
+            id = 0;
+            messages.set(holder.toString() + ".received", null);
+        }
+        messages.set(holder.toString() + ".received." + id + ".from", from);
+        messages.set(holder.toString() + ".received." + id + ".msg", msg);
+        saveMessages();
+    }
+
+    public String getMessages(UUID holder){
+        StringBuilder sb = new StringBuilder();
+        if (!messages.isSet(holder.toString())){
+            return "No messages found";
+        }
+        ConfigurationSection section = messages.getConfigurationSection(holder.toString() + ".sent");
+        for(String s: section.getKeys(false)){
+            int id = Integer.parseInt(s);
+            sb.append("->");
+            sb.append(getSentMessageById(holder, id));
+            sb.append("\n");
+        }
+        section = messages.getConfigurationSection(holder.toString() + ".received");
+        for(String s: section.getKeys(false)){
+            int id = Integer.parseInt(s);
+            sb.append("<-");
+            sb.append(getReceivedMessageById(holder, id));
+            sb.append("\n");
+        }
+        if (sb.length()<=2){
+            return "No messages found";
+        }
+        return sb.toString();
+    }
+
+    public String getSentMessageById(UUID holder, int id){
+        String path = holder.toString() + ".sent." + id;
+        if (!messages.isSet(path + ".to") || !messages.isSet(path + ".msg")){
+            return "";
+        }
+        String to =  messages.getString(path + ".to");
+        String msg = messages.getString(path + ".msg");
+        return to + " : " + msg;
+    }
+    public String getReceivedMessageById(UUID holder, int id){
+        String path = holder.toString() + ".received." + id;
+        if (!messages.isSet(path + ".from") || !messages.isSet(path + ".msg")){
+            return "";
+        }
+        String from =  messages.getString(path + ".from");
+        String msg = messages.getString(path + ".msg");
+        return from + " : " + msg;
+    }
 }
